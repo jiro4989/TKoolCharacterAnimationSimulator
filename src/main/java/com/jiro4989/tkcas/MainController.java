2be4a516456c892a046d6ec040d748e62e175a2d
jiro4989 <jiroron666@gmail.com>
2020-08-15 10:23:02 +0000

    一旦コンパイルが通る状態にした

--- src/main/java/com/jiro4989/tkcas/MainController.java
+++ src/main/java/com/jiro4989/tkcas/MainController.java
@@ -4,9 +4,8 @@ import static com.jiro4989.tkcas.util.Texts.*;
 import static java.util.stream.IntStream.range;
 
 import com.jiro4989.tkcas.config.ConfigStage;
-import com.jiro4989.tkcas.layout.PositionsFlowPane;
-import com.jiro4989.tkcas.menubar.MyMenuBar;
 import com.jiro4989.tkcas.menubar.TrimmingSelector;
+import com.jiro4989.tkcas.stage.MyFileChooser;
 import com.jiro4989.tkcas.standard.Standards;
 import com.jiro4989.tkcas.util.DialogUtils;
 import com.jiro4989.tkcas.util.MyProperties;
@@ -14,7 +13,9 @@ import com.jiro4989.tkcas.util.OpenRecentFilesUtils;
 import java.io.*;
 import java.util.*;
 import java.util.regex.*;
+import java.util.stream.Collectors;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -41,8 +42,7 @@ public class MainController {
   private final MyProperties walkLog = new MyProperties(LOG_FILE_WALK);
   private final MyProperties sideViewLog = new MyProperties(LOG_FILE_SIDE_VIEW);
 
-  @FXML private MyMenuBar myMenuBar;
-  @FXML private PositionsFlowPane positionsFlowPane;
+  // @FXML private PositionsFlowPane positionsFlowPane;
 
   // 初期化
 
@@ -50,7 +50,6 @@ public class MainController {
   private void initialize() {
 
     preferences.load();
-    myMenuBar.setMainController(this);
 
     // TODO 一時的な設定
     String walk = WALK_PREST;
@@ -85,7 +84,7 @@ public class MainController {
           .filter(f -> p.matcher(f.getName()).matches())
           .forEach(
               file -> {
-                myMenuBar.setDisables(false);
+                // myMenuBar.setDisables(false);
 
                 String result = DialogUtils.showChoiseDialog();
                 if (result.equals("w")) {
@@ -94,11 +93,11 @@ public class MainController {
                   ts.showAndWait();
 
                   drawWalkImage(file);
-                  myMenuBar.addRecentWalkFile(file);
+                  // myMenuBar.addRecentWalkFile(file);
                   return;
                 }
                 drawSideViewImage(file);
-                myMenuBar.addRecentSideViewFile(file);
+                // myMenuBar.addRecentSideViewFile(file);
               });
     }
   }
@@ -116,7 +115,7 @@ public class MainController {
     fileObserver = new FileObserver(200, file, this);
     strategy = new WalkGraphicsStrategy(this);
 
-    positionsFlowPane.drawWalkImage(file.getPath(), walkStandard);
+    // positionsFlowPane.drawWalkImage(file.getPath(), walkStandard);
 
     configStageOpt.ifPresent(
         cs -> {
@@ -132,7 +131,7 @@ public class MainController {
     fileObserver = new FileObserver(200, file, this);
     strategy = new SideViewStrategy(this);
 
-    positionsFlowPane.drawSideViewImage(file.getPath(), sideViewStandard);
+    // positionsFlowPane.drawSideViewImage(file.getPath(), sideViewStandard);
 
     configStageOpt.ifPresent(
         cs -> {
@@ -142,7 +141,7 @@ public class MainController {
   }
 
   public void clearImages() {
-    positionsFlowPane.clearImages();
+    // positionsFlowPane.clearImages();
   }
 
   public void showConfigStage() {
@@ -157,7 +156,7 @@ public class MainController {
 
   public void updateZoomRate(double zoom) {
 
-    positionsFlowPane.updateZoomRate(zoom);
+    // positionsFlowPane.updateZoomRate(zoom);
   }
 
   public void updateZoomRate(ScrollEvent e) {
@@ -170,7 +169,7 @@ public class MainController {
 
   public void updateAnimationSpeed(int duration) {
 
-    positionsFlowPane.updateAnimationSpeed(duration);
+    // positionsFlowPane.updateAnimationSpeed(duration);
   }
 
   public void changeAlwaysOnTop() {
@@ -182,11 +181,11 @@ public class MainController {
   }
 
   public void showPreviousImage() {
-    positionsFlowPane.showPreviousImage();
+    // positionsFlowPane.showPreviousImage();
   }
 
   public void showNextImage() {
-    positionsFlowPane.showNextImage();
+    // positionsFlowPane.showNextImage();
   }
 
   public void zoomDownImages() {
@@ -206,16 +205,16 @@ public class MainController {
   }
 
   public void showOneImage() {
-    positionsFlowPane.switchViewerMode();
+    // positionsFlowPane.switchViewerMode();
   }
 
   public void closeRequest() {
 
     // 最近開いた画像を保存
-    List<String> openedWalkFiles = myMenuBar.getRecentOpenedWalkFiles();
-    List<String> openedSideViewFiles = myMenuBar.getRecentOpenedSideViewFiles();
-    storeRecentFile(walkLog, openedWalkFiles);
-    storeRecentFile(sideViewLog, openedSideViewFiles);
+    // List<String> openedWalkFiles = myMenuBar.getRecentOpenedWalkFiles();
+    // List<String> openedSideViewFiles = myMenuBar.getRecentOpenedSideViewFiles();
+    // storeRecentFile(walkLog, openedWalkFiles);
+    // storeRecentFile(sideViewLog, openedSideViewFiles);
 
     configStageOpt.ifPresent(
         cs -> {
@@ -229,7 +228,7 @@ public class MainController {
     preferences.setProperty(KEY_ALWAYS_ON_TOP, "" + alwaysOnTop);
     preferences.store();
 
-    Main.mainMp.setProperty(myMenuBar);
+    // Main.mainMp.setProperty(myMenuBar);
     Main.mainMp.store();
   }
 
@@ -239,7 +238,7 @@ public class MainController {
 
     configStageOpt.ifPresent(
         c -> {
-          c.resize(positionsFlowPane);
+          // c.resize(positionsFlowPane);
         });
   }
 
@@ -260,8 +259,8 @@ public class MainController {
   }
 
   private Stage getStage() {
-
-    return (Stage) positionsFlowPane.getScene().getWindow();
+    return null;
+    // return (Stage) positionsFlowPane.getScene().getWindow();
   }
 
   private void setConfigs() {
@@ -306,8 +305,8 @@ public class MainController {
 
   public void setFontSize(String fontSize) {
 
-    VBox root = (VBox) positionsFlowPane.getScene().lookup("#root");
-    root.setStyle("-fx-font-size:" + fontSize + "pt;");
+    // VBox root = (VBox) positionsFlowPane.getScene().lookup("#root");
+    // root.setStyle("-fx-font-size:" + fontSize + "pt;");
     preferences.setProperty(KEY_FONT_SIZE, fontSize);
   }
 
@@ -318,7 +317,7 @@ public class MainController {
 
   public void setWalkStandard(File file) {
 
-    myMenuBar.setCurrentWalkPreset(file);
+    // myMenuBar.setCurrentWalkPreset(file);
 
     String path = file.getPath();
     walkStandard = WalkGraphicsStrategy.createStandard(path);
@@ -331,7 +330,7 @@ public class MainController {
 
   public void setSideViewStandard(File file) {
 
-    myMenuBar.setCurrentSideViewPreset(file);
+    // myMenuBar.setCurrentSideViewPreset(file);
 
     String path = file.getPath();
     sideViewStandard = SideViewStrategy.createStandard(path);
@@ -343,8 +342,8 @@ public class MainController {
   }
 
   void setConfigStageInstance() {
-    ConfigStage cs = new ConfigStage(positionsFlowPane, this);
-    configStageOpt = Optional.ofNullable(cs);
+    // ConfigStage cs = new ConfigStage(positionsFlowPane, this);
+    // configStageOpt = Optional.ofNullable(cs);
     setConfigs();
   }
 
@@ -353,20 +352,437 @@ public class MainController {
     String a = preferences.getProperty(KEY_ALWAYS_ON_TOP).orElse("false");
     boolean alwaysOnTop = Boolean.valueOf(a);
     getStage().setAlwaysOnTop(alwaysOnTop);
-    myMenuBar.setAlwaysOnTop(alwaysOnTop);
-  }
-
-  void setFontSizeOfMenuBar(String fontSize) {
-
-    myMenuBar.setFontSizeOfMenuBar(fontSize);
+    // myMenuBar.setAlwaysOnTop(alwaysOnTop);
   }
 
   void setRecentFiles() {
 
     List<String> walkList = createRecentListWith(walkLog);
-    myMenuBar.setRecentWalkFilePaths(walkList);
+    // myMenuBar.setRecentWalkFilePaths(walkList);
 
     List<String> sideViewList = createRecentListWith(sideViewLog);
-    myMenuBar.setRecentSideViewFilePaths(sideViewList);
+    // myMenuBar.setRecentSideViewFilePaths(sideViewList);
+  }
+
+  private MyFileChooser imagefileChooser;
+  private MyFileChooser walkPresetFileChooser;
+  private MyFileChooser sideViewPresetFileChooser;
+
+  // ファイル
+  @FXML private Menu fileMenu;
+  @FXML private MenuItem openCharaChipMenuItem;
+  @FXML private MenuItem openSideViewMenuItem;
+  @FXML private Menu openWalkRecentMenu;
+  @FXML private Menu openSideViewRecentMenu;
+  @FXML private MenuItem closeMenuItem;
+  @FXML private MenuItem currentWalkPresetMenuItem;
+  @FXML private MenuItem currentSideViewPresetMenuItem;
+  @FXML private MenuItem walkPresetMenuItem;
+  @FXML private MenuItem sideViewPresetMenuItem;
+  @FXML private MenuItem editWalkPresetMenuItem;
+  @FXML private MenuItem editSideViewPresetMenuItem;
+  @FXML private MenuItem preferencesMenuItem;
+  @FXML private MenuItem quitMenuItem;
+  @FXML private MenuItem forcedTerminateMenuItem;
+
+  // 表示
+  @FXML private Menu displayMenu;
+  @FXML private MenuItem previousMenuItem;
+  @FXML private MenuItem nextMenuItem;
+  @FXML private MenuItem zoomDownMenuItem;
+  @FXML private MenuItem zoomUpMenuItem;
+  @FXML private MenuItem durationDownMenuItem;
+  @FXML private MenuItem durationUpMenuItem;
+  @FXML private MenuItem onlyMenuItem;
+  @FXML private CheckMenuItem alwaysOnTopMenuItem;
+
+  // フォントサイズ変更メニュー
+  @FXML private Menu fontMenu;
+  @FXML private ToggleGroup fontGroup;
+  @FXML private RadioMenuItem fontSize8RadioMenuItem;
+  @FXML private RadioMenuItem fontSize9RadioMenuItem;
+  @FXML private RadioMenuItem fontSize10RadioMenuItem;
+  @FXML private RadioMenuItem fontSize11RadioMenuItem;
+  @FXML private RadioMenuItem fontSize12RadioMenuItem;
+
+  // 言語変更メニュー
+  @FXML private Menu langsMenu;
+  @FXML private ToggleGroup langGroup;
+  @FXML private RadioMenuItem jpRadioMenuItem;
+  @FXML private RadioMenuItem usRadioMenuItem;
+
+  // コンストラクタ
+
+  // public MyMenuBar() {
+
+  //   imagefileChooser = new MyFileChooser.Builder("Image Files", "*.png").build();
+  //   walkPresetFileChooser =
+  //       new MyFileChooser.Builder("Preset Files", "*.preset").initDir(WALK_PREST_DIR).build();
+  //   sideViewPresetFileChooser =
+  //       new MyFileChooser.Builder("Preset Files",
+  // "*.preset").initDir(SIDE_VIEW_PREST_DIR).build();
+
+  //   URL location = getClass().getResource("my_menu_bar.fxml");
+  //   ResourceBundle resources =
+  //       ResourceBundle.getBundle(
+  //           "com.jiro4989.tkcas.menubar.dict",
+  //           Locale.getDefault(),
+  //           ResourceBundleWithUtf8.UTF8_ENCODING_CONTROL);
+  //   FXMLLoader loader = new FXMLLoader(location, resources);
+
+  //   loader.setRoot(this);
+  //   loader.setController(this);
+
+  //   try {
+  //     loader.load();
+
+  //     fontSize8RadioMenuItem.setOnAction(e -> setFontSize(fontSize8RadioMenuItem));
+  //     fontSize9RadioMenuItem.setOnAction(e -> setFontSize(fontSize9RadioMenuItem));
+  //     fontSize10RadioMenuItem.setOnAction(e -> setFontSize(fontSize10RadioMenuItem));
+  //     fontSize11RadioMenuItem.setOnAction(e -> setFontSize(fontSize11RadioMenuItem));
+  //     fontSize12RadioMenuItem.setOnAction(e -> setFontSize(fontSize12RadioMenuItem));
+
+  //     changeSelectedFontMenuItem();
+
+  //   } catch (IOException e) {
+  //     e.printStackTrace();
+  //   }
+  // }
+
+  // FXMLイベント
+  // ファイルメニュー
+  @FXML
+  private void openCharaChipMenuItemOnAction() {
+
+    imagefileChooser
+        .openFile()
+        .ifPresent(
+            file -> {
+              // Standards std = mainController.getWalkStandards();
+              // TrimmingSelector ts = new TrimmingSelector(file, std);
+              // ts.showAndWait();
+
+              // setDisables(false);
+              // mainController.drawWalkImage(file);
+
+              // String path = file.getPath();
+              // openWalkRecentMenu.getItems().add(createMenuItemHasWalkAction(path));
+            });
+  }
+
+  @FXML
+  private void openSideViewMenuItemOnAction() {
+    imagefileChooser
+        .openFile()
+        .ifPresent(
+            file -> {
+              // setDisables(false);
+              // mainController.drawSideViewImage(file);
+
+              // String path = file.getPath();
+              // openSideViewRecentMenu.getItems().add(createMenuItemHasSideViewAction(path));
+            });
+  }
+
+  @FXML
+  private void openWalkRecentMenuItemOnAction() {
+    imagefileChooser
+        .openFile()
+        .ifPresent(
+            file -> {
+              // // FIXME NullPointerexception
+              // mainController.drawWalkImage(file);
+            });
+  }
+
+  @FXML
+  private void openSideViewRecentMenuItemOnAction() {
+    imagefileChooser
+        .openFile()
+        .ifPresent(
+            file -> {
+              // // FIXME NullPointerexception
+              // mainController.drawWalkImage(file);
+            });
+  }
+
+  @FXML
+  private void closeMenuItemOnAction() {
+    // mainController.clearImages();
+    // setDisables(true);
+  }
+
+  @FXML
+  private void walkPresetMenuItemOnAction() {
+    walkPresetFileChooser
+        .openFile()
+        .ifPresent(
+            file -> {
+              // mainController.setWalkStandard(file);
+              // setCurrentWalkPreset(file);
+            });
+  }
+
+  @FXML
+  private void sideViewPresetMenuItemOnAction() {
+    sideViewPresetFileChooser
+        .openFile()
+        .ifPresent(
+            file -> {
+              // mainController.setSideViewStandard(file);
+              // setCurrentSideViewPreset(file);
+            });
+  }
+
+  @FXML
+  private void editWalkPresetMenuItemOnAction() {
+    walkPresetFileChooser
+        .saveFile()
+        .ifPresent(
+            file -> {
+              // PresetEditor editor = new PresetEditor(file);
+              // editor.showAndWait();
+              // mainController.setWalkStandard(file);
+              // setCurrentWalkPreset(file);
+            });
+  }
+
+  @FXML
+  private void editSideViewPresetMenuItemOnAction() {
+    sideViewPresetFileChooser
+        .saveFile()
+        .ifPresent(
+            file -> {
+              // SideViewEditor editor = new SideViewEditor(file);
+              // editor.showAndWait();
+              // mainController.setSideViewStandard(file);
+              // setCurrentSideViewPreset(file);
+            });
+  }
+
+  @FXML
+  private void preferencesMenuItemOnAction() {
+    // mainController.showConfigStage();
+  }
+
+  @FXML
+  private void quitMenuItemOnAction() {
+    // mainController.closeRequest();
+    // Platform.exit();
+  }
+
+  @FXML
+  private void forcedTerminateMenuItemOnAction() {
+    DialogUtils.showForcedTerminationDialog();
+  }
+  // 表示メニュー
+  @FXML
+  private void alwaysOnTopMenuItemOnAction() {
+    // mainController.changeAlwaysOnTop();
+  }
+
+  @FXML
+  private void previousMenuItemOnAction() {
+    // mainController.showPreviousImage();
+  }
+
+  @FXML
+  private void nextMenuItemOnAction() {
+    // mainController.showNextImage();
+  }
+
+  @FXML
+  private void zoomDownMenuItemOnAction() {
+
+    // mainController.zoomDownImages();
+  }
+
+  @FXML
+  private void zoomUpMenuItemOnAction() {
+
+    // mainController.zoomUpImages();
+  }
+
+  @FXML
+  private void durationDownMenuItemOnAction() {
+    // mainController.durationDown();
+  }
+
+  @FXML
+  private void durationUpMenuItemOnAction() {
+    // mainController.durationUp();
+  }
+
+  @FXML
+  private void onlyMenuItemOnAction() {
+
+    // mainController.showOneImage();
+  }
+  // 言語メニュー
+  @FXML
+  private void jpRadioMenuItemOnAction() {
+
+    // DialogUtils.showLanguageDialog();
+    // String langs = Locale.JAPAN.getLanguage();
+    // mainController.setLanguages(langs);
+  }
+
+  @FXML
+  private void usRadioMenuItemOnAction() {
+
+    // DialogUtils.showLanguageDialog();
+    // String langs = Locale.ENGLISH.getLanguage();
+    // mainController.setLanguages(langs);
+  }
+
+  // private メソッド
+  private void setFontSize(RadioMenuItem rmi) {
+    // String fontSize = rmi.getText();
+    // mainController.setFontSize(fontSize);
+  }
+
+  private void setPresetText(MenuItem item, String text) {
+
+    String current = item.getText();
+    String top = current.split(":")[0];
+    String newText = top + ": " + text;
+    item.setText(newText);
+  }
+
+  private void changeSelectedFontMenuItem() {
+    String defaultLanguage = Locale.getDefault().getLanguage();
+    String ja = Locale.JAPAN.getLanguage();
+
+    if (defaultLanguage.equals(ja)) jpRadioMenuItem.setSelected(true);
+    else usRadioMenuItem.setSelected(true);
+  }
+
+  private MenuItem createMenuItemHasWalkAction(String path) {
+
+    MenuItem item = new MenuItem(path);
+    item.setOnAction(
+        e -> {
+          File file = new File(item.getText());
+
+          if (file.exists()) {
+
+            // Standards std = mainController.getWalkStandards();
+            // TrimmingSelector ts = new TrimmingSelector(file, std);
+            // ts.showAndWait();
+
+            // setDisables(false);
+            // mainController.drawWalkImage(file);
+          }
+        });
+
+    return item;
+  }
+
+  private MenuItem createMenuItemHasSideViewAction(String path) {
+
+    MenuItem item = new MenuItem(path);
+    item.setOnAction(
+        e -> {
+          // setDisables(false);
+          // File file = new File(item.getText());
+          // if (file.exists()) mainController.drawSideViewImage(file);
+        });
+
+    return item;
+  }
+  // Getter
+  public List<String> getRecentOpenedWalkFiles() {
+
+    return openWalkRecentMenu
+        .getItems()
+        .stream()
+        .map(item -> item.getText())
+        .collect(Collectors.toList());
+  }
+
+  public List<String> getRecentOpenedSideViewFiles() {
+
+    return openSideViewRecentMenu
+        .getItems()
+        .stream()
+        .map(item -> item.getText())
+        .collect(Collectors.toList());
+  }
+  // Setter
+  public void setFontSizeOfMenuBar(String fontSize) {
+    fontGroup
+        .getToggles()
+        .stream()
+        .map(t -> (RadioMenuItem) t)
+        .filter(t -> t.getText().equals(fontSize))
+        .forEach(t -> t.setSelected(true));
+  }
+
+  public void setRecentWalkFilePaths(List<String> paths) {
+
+    paths
+        .stream()
+        .distinct()
+        .filter(p -> new File(p).exists())
+        .map(this::createMenuItemHasWalkAction)
+        .forEach(
+            item -> {
+              openWalkRecentMenu.getItems().add(item);
+            });
+  }
+
+  public void setRecentSideViewFilePaths(List<String> paths) {
+
+    paths
+        .stream()
+        .distinct()
+        .filter(p -> new File(p).exists())
+        .map(this::createMenuItemHasSideViewAction)
+        .forEach(
+            item -> {
+              openSideViewRecentMenu.getItems().add(item);
+            });
+  }
+
+  public void setAlwaysOnTop(boolean alwaysOnTop) {
+    alwaysOnTopMenuItem.setSelected(alwaysOnTop);
+  }
+
+  public void setCurrentWalkPreset(String text) {
+    setPresetText(currentWalkPresetMenuItem, text);
+  }
+
+  public void setCurrentWalkPreset(File file) {
+    setPresetText(currentWalkPresetMenuItem, file.getName());
+  }
+
+  public void setCurrentSideViewPreset(String text) {
+    setPresetText(currentSideViewPresetMenuItem, text);
+  }
+
+  public void setCurrentSideViewPreset(File file) {
+    setPresetText(currentSideViewPresetMenuItem, file.getName());
+  }
+
+  public void setDisables(boolean disable) {
+
+    onlyMenuItem.setDisable(disable);
+    previousMenuItem.setDisable(disable);
+    nextMenuItem.setDisable(disable);
+    zoomDownMenuItem.setDisable(disable);
+    zoomUpMenuItem.setDisable(disable);
+    durationDownMenuItem.setDisable(disable);
+    durationUpMenuItem.setDisable(disable);
+    closeMenuItem.setDisable(disable);
+  }
+
+  public void addRecentWalkFile(File file) {
+    MenuItem item = createMenuItemHasWalkAction(file.getPath());
+    openWalkRecentMenu.getItems().add(item);
+  }
+
+  public void addRecentSideViewFile(File file) {
+    MenuItem item = createMenuItemHasSideViewAction(file.getPath());
+    openSideViewRecentMenu.getItems().add(item);
   }
 }

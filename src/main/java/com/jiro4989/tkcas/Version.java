d83e3fa2655badbf8fb9143b0b889d868b59c71d
jiro <jiroron666@gmail.com>
2020-08-15 18:34:18 +0900

    chore: バージョン番号をCIビルド時にセットするように変更 #7 (#19)

---
+++ src/main/java/com/jiro4989/tkcas/Version.java
@@ -0,0 +1,6 @@
+package com.jiro4989.tkcas;
+
+public class Version {
+  public static final String version = "dev";
+  public static final String commitHash = "dev";
+}
---
+++ template/Version.java
@@ -0,0 +1,6 @@
+package com.jiro4989.tkcas;
+
+public class Version {
+  public static final String version = "$version";
+  public static final String commitHash = "$commitHash";
+}
